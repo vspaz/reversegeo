3040e865f1425c6f002f8b6cd292111073b5d39f
vspaz <vladimir.edelman@gmail.com>
2021-11-19 21:00:33 +0100

    add logger

--- reversegeo/reversegeo.py
+++ reversegeo/reversegeo.py
@@ -1,5 +1,4 @@
 import json
-import logging
 import os
 import sys
 
@@ -8,40 +7,23 @@ from filters import get_european_country_and_city
 from googleapiclient import GoogleApiClient
 from reversegeo import cli
 from reversegeo.__version__ import __version__
-
-
-def configure_logger(logger_config):
-    params = {
-        'stream': sys.stderr,
-        'level': logging.INFO,
-        'format': '%(asctime)s.%(msecs)03d %(levelname)s: %(message)s',
-        'datefmt': '%Y-%m-%d %X',
-    }
-
-    if logger_config:
-        logdir = logger_config.get('logdir', '.')
-        log_file_name = logger_config.get('log_file_name')
-        if log_file_name:
-            params['filename'] = os.path.join(logdir, log_file_name)
-            del params['stream']
-
-    logging.basicConfig(**params)
+from reversegeo.log import configure_logger
 
 
 def main():
     args = cli.get_args()
     config = json.loads(args.config)
 
-    configure_logger(logger_config=config.get('logging'))
-    logging.info(f'reversegeo {__version__}!r')
-    logging.info(f'PID = {os.getpid()}')
+    logger = configure_logger(logger_config=config.get('logging'))
+    logger.info(f'reversegeo {__version__!r}')
+    logger.info(f'PID = {os.getpid()}')
 
     gclient = GoogleApiClient(config=config)
-    logging.info('Trying to read from the csv source file(s)')
+    logger.info('Trying to read from the csv source file(s)')
 
     csv_files = get_files(dirs=args.source, pattern=args.files)
     coordinates = from_files(files=csv_files)
-    logging.info('Processing started')
+    logger.info('Processing started')
     total_count = 0
     for coord in coordinates:
         try:
@@ -57,14 +39,14 @@ def main():
             )
             to_csv(file_path=dump_file_path, fields=fields)
         except KeyboardInterrupt:
-            logging.info('Processing interrupted')
+            logger.info('Processing interrupted')
             sys.exit(0)
         except Exception as e:
-            logging.error('%s for coordinates %s', e, coord)
+            logger.error('%s for coordinates %s', e, coord)
         total_count += 1
-    logging.info('Processing complete')
-    logging.info('Processed %s records', total_count)
-    logging.info('Shut down')
+    logger.info('Processing complete')
+    logger.info('Processed %s records', total_count)
+    logger.info('Shut down')
 
 
 if __name__ == '__main__':
